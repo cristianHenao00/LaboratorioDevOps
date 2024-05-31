2b9751f1e0a631dec558ace90aa0a8c449028709
CristianHenao <cristianhenao88@gmail.com>
2024-05-30 20:36:00 -0500

    project config

---
+++ suma.test.js
@@ -0,0 +1,9 @@
+import suma from "./suma";
+
+test("adds 1 + 2 to equal 3", () => {
+  expect(suma(1, 2)).toBe(3);
+});
+
+test("adds a + 2 to equal NaN", () => {
+  expect(suma("1", 2)).toBe(NaN);
+});
