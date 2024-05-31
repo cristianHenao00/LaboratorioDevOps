a913ea202181ebe985dbf508105b6299f5d8ad06
CristianHenao <cristianhenao88@gmail.com>
2024-05-30 20:45:51 -0500

    modification suma.test.js v1

--- suma.test.js
+++ suma.test.js
@@ -1,7 +1,7 @@
 const suma = require("./suma");
 
 test("adds 1 + 2 to equal 3", () => {
-  expect(suma(1, 2)).toBe(3);
+  expect(suma(1, 4)).toBe(3);
 });
 
 test("adds a + 2 to equal NaN", () => {
